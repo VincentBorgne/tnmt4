1c44bc8594b9c88e44f8d2505e92e5a69b7f2573
VincentBorgne <vrborgne@gmail.com>
2025-11-14 19:33:24 +0800

    1.1 simpler form

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import TournamentRegistration from './components/TournamentRegistration';
 import RegistrationReport from './components/RegistrationReport';
+
+// Component to determine background color based on tournament
+function AppContent() {
+  const location = useLocation();
+  
+  // Determine tournament from URL path
+  const isMPATournament = location.pathname.includes('mpa-dec-2025');
+  const bgColor = isMPATournament ? 'bg-white' : 'bg-black';
+  
+  return (
+    <div className={`min-h-screen ${bgColor} py-8 px-4 sm:px-6 lg:px-8`}>
+      <div className="max-w-3xl mx-auto">
+        <Routes>
+          {/* Registration routes - slug from iframe URL determines which tournament */}
+          <Route path="/register/:tournamentId" element={<TournamentRegistration />} />
+          
+          {/* Report route */}
+          <Route path="/registrations" element={<RegistrationReport />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
 
 export function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-black py-8 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-3xl mx-auto">
-          <Routes>
-            {/* Registration routes - slug from iframe URL determines which tournament */}
-            <Route path="/register/:tournamentId" element={<TournamentRegistration />} />
-            
-            {/* Report route */}
-            <Route path="/registrations" element={<RegistrationReport />} />
-          </Routes>
-        </div>
-      </div>
+      <AppContent />
     </BrowserRouter>
   );
 }
